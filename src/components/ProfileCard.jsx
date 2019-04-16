4c848ce4f6a6609b2d8f20b59a65e2605ee28320
hiufungk <hiufungk@ualberta.ca>
2019-04-16 03:19:09 -0600

    added skills in about me section

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -11,22 +11,36 @@ import ExpansionPanelSummary
 import ExpansionPanelDetails
     from "@material-ui/core/ExpansionPanelDetails/ExpansionPanelDetails";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import Flag from 'react-world-flags'
+import Grid from "@material-ui/core/Grid/Grid";
 
 const styles = theme => ({
     chip: {
         margin: theme.spacing.unit,
+    },
+    row: {
+        margin: theme.spacing.unit,
     }
-
 });
 
 class ProfileCard extends Component {
 
+    isMobile() {
+       if(window.innerWidth <= 800 && window.innerHeight <= 800) {
+         return true;
+       } else {
+         return false;
+       }
+    }
+
     render() {
-        const {classes} = this.props;
+        const {classes} = this.props,
+            isDesktop = !this.isMobile();
         return (
             <div className={classes.root}>
                 <Paper className={classes.paper}>
-                <ExpansionPanel defaultExpanded>
+                <ExpansionPanel defaultExpanded={isDesktop}>
                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography>About Me</Typography>
                     </ExpansionPanelSummary>
@@ -37,28 +51,102 @@ class ProfileCard extends Component {
                         </Typography>
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
-                <ExpansionPanel defaultExpanded>
+                <ExpansionPanel defaultExpanded={isDesktop}>
                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography>Skills</Typography>
                     </ExpansionPanelSummary>
                     <Divider/>
                     <ExpansionPanelDetails>
-                        <Chip label={"aaaaaaaaaaaaaaaaaaa"} color="secondary" className={classes.chip}/>
-                        <Chip label={"aaaaaaaaaaaaaaaaaaa"} color="secondary" className={classes.chip}/>
-                        <Chip label={"aaaaaaaaaaaaaaaaaaa"} color="secondary" className={classes.chip}/>
-                        <Chip label={"aaaaaaaaaaaaaaaaaaa"} color="secondary" className={classes.chip}/>
-                        <Chip label={"aaaaaaaaaaaaaaaaaaa"} color="secondary" className={classes.chip}/>
+                        <Grid
+                            container
+                            spacing={16}
+                            alignItems={"flex-start"}
+                            direction={'column'}
+                            justify={"flex-start"}
+                        >
+                        <Grid
+                            className={classes.row}
+                            container
+                            spacing={16}
+                            alignItems={"flex-start"}
+                            direction={'row'}
+                            justify={"flex-start"}
+                        >
+                            <Chip label={"Python"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Java"} color="secondary" className={classes.chip}/>
+                            <Chip label={"C++"} color="secondary" className={classes.chip}/>
+                            <Chip label={"JavaScript"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Excel VBA"} color="secondary" className={classes.chip}/>
+                        </Grid>
+                        <Grid
+                            className={classes.row}
+                            container
+                            spacing={16}
+                            alignItems={"flex-start"}
+                            direction={'row'}
+                            justify={"flex-start"}
+                        >
+                            <Chip label={"Web"} color="secondary" className={classes.chip}/>
+                            <Chip label={"React"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Android"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Google Maps"} color="secondary" className={classes.chip}/>
+                            <Chip label={"SQL"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Object Relational Mapping"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Django"} color="secondary" className={classes.chip}/>
+                        </Grid>
+                        <Grid
+                            className={classes.row}
+                            container
+                            spacing={16}
+                            alignItems={"flex-start"}
+                            direction={'row'}
+                            justify={"flex-start"}
+                        >
+                            <Chip label={"Machine Learning"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Reinforcement Learning"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Dynamic Programming"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Object Oriented Design"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Algorithm Design"} color="secondary" className={classes.chip}/>
+                        </Grid>
+                        <Grid
+                            className={classes.row}
+                            container
+                            spacing={16}
+                            alignItems={"flex-start"}
+                            direction={'row'}
+                            justify={"flex-start"}
+                        >
+                            <Chip label={"Continuous Integration"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Agile"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Test Driven Development"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Poker Planning"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Sprint Cycles"} color="secondary" className={classes.chip}/>
+                            <Chip label={"Client Interaction"} color="secondary" className={classes.chip}/>
+                        </Grid>
+                        </Grid>
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
-                <ExpansionPanel defaultExpanded>
+                <ExpansionPanel defaultExpanded={isDesktop}>
                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography>Languages</Typography>
                     </ExpansionPanelSummary>
                     <Divider/>
                     <ExpansionPanelDetails>
-                        <Chip label={"English"} color="secondary" className={classes.chip}/>
-                        <Chip label={"Cantonese"} color="secondary" className={classes.chip}/>
-                        <Chip label={"Mandarin"} color="secondary" className={classes.chip}/>
+                        <Chip
+                            avatar={<Avatar><Flag code="GB"/></Avatar>}
+                            label={"English"}
+                            className={classes.chip}
+                        />
+                        <Chip
+                            avatar={<Avatar><Flag code="HK"/></Avatar>}
+                            label={"Cantonese"}
+                            className={classes.chip}
+                        />
+                        <Chip
+                            avatar={<Avatar><Flag code="CN"/></Avatar>}
+                            label={"Mandarin"}
+                            className={classes.chip}
+                        />
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
 
