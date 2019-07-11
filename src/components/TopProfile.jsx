a44318b0fd75d8401bf6643efad6ca3637ba63ab
hiufungk <hiufungk@ualberta.ca>
2019-07-11 14:27:35 -0600

    Update TopProfile.jsx

--- src/components/TopProfile.jsx
+++ src/components/TopProfile.jsx
@@ -63,7 +63,7 @@ class TopProfile extends Component {
                     {/*</Typography>*/}
                     <Typography align="center" variant="body1" color="textSecondary" gutterBottom>
                         {/*<img src={uofaLogo} alt="University of Alberta" height="80" width="80"></img> */}
-                        BSc with Specialization Computing Science – Software Practice
+                        BSc with Specialization in Computing Science – Software Practice – Science Internship Program with Distinction
                     </Typography>
                     {/*<Typography variant="body1" gutterBottom>*/}
                         {/*Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum*/}
